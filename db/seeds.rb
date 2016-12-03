d25b87e0dbed2a8e289eaf2e14792c2f73abe082
accel <accel194@gmail.com>
2016-12-03 07:12:17 +0000

    update_Peripheral

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times do |index|
+    Customer.create(family_name: "藤井", given_name: "清行", email: "customer_#(index)@sparta.com")
+end
